8befb2d31ea15f14c4acfc57111d0fc3516b194c
Lorenzo Planas <lorenzo.planas@gmail.com>
2013-08-15 18:42:02 +0200

    Generate sample tasks

--- js/tasks.js
+++ js/tasks.js
@@ -1,24 +1,55 @@
-$('#lists .task').bind('dragstart', function(event) {
+var on_dragstart = function(event) {
   var id = event.target.getAttribute('id');
   event.originalEvent.dataTransfer.setData("text/plain", id);
-});
+}
 
-$('#lists ul').bind('dragover', function(event) {
+var on_dragover = function(event) {
   event.preventDefault();
-});
+}
 
-$('#lists ul').bind('drop', function(event) {
+var on_drop = function(event) {
   event.preventDefault();
   var id      = event.originalEvent.dataTransfer.getData("text/plain");
-  var element = document.getElementById(id);
+  var task    = document.getElementById(id);
   var target  = $(event.target);
 
-  if (target.prop('tagName').toLowerCase() == 'ul') {
-    target.append(element);
-  }
+  drop_task(target, task);
+}
 
-  if (target.prop('tagName').toLowerCase() == 'li') {
-    target.after(element);
+var create_task = function(list, task_name, id) {
+  task = $('<li/>').html(task_name)
+          .prop('id', id)
+          .prop('draggable', true)
+          .addClass('task');
+
+  task.bind('dragstart', on_dragstart);
+  list.append(task);
+}
+
+var generate_sample_tasks = function(lists) {
+  for (i = 1; i <= lists.length; i++) {
+    list = $(lists[i - 1]);
+
+    for (j = 1; j < 4; j ++) {
+      task_id   = i + '.' + j;
+      task_name = 'Task ' + task_id;
+      create_task(list, task_name, task_id);
+    }
   }
-});
+}
+
+var drop_task = function(target, task) {
+  var tag = target.prop('tagName').toLowerCase()
+  if (tag == 'ul')
+    target.append(task);
+
+  if (tag == 'li')
+    target.after(task);
+}
+
+var lists = $('#lists ul');
+generate_sample_tasks(lists);
+
+lists.bind('dragover', on_dragover);
+lists.bind('drop', on_drop);
 
